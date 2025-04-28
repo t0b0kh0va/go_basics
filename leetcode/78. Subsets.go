38baacba7e0dbc74ace8d5b209985038078f2ac0
sargylanatobokhova <sargylana.tobokhova@mytona.com>
2025-04-29 01:35:34 +0900

    leetcode

---
+++ leetcode/78. Subsets.go
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/subsets/
+//#array #backtracking #bitmanipulation
+func subsets(nums []int) [][]int {
+	func subsets(nums []int) [][]int {
+		var ans [][]int
+		n := len(nums)
+		for st := 0; st < (1 << n); st++ {
+			subset := []int{}
+			for i := 0; i < n; i++ {
+				if st&(1<<i) != 0 {
+					subset = append(subset, nums[i])
+				}
+			}
+			ans = append(ans, subset)
+		}
+		return ans
+	}
